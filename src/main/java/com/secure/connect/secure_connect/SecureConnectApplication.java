3e5a9a3f21cd0c342c2a7ccf055938d93dca74cb
Vinicius Santos Carneiro <viniciucarneiro@gmail.com>
2025-02-08 23:22:05 -0300

    Implementando estrutura do projeto

---
+++ src/main/java/com/secure/connect/secure_connect/SecureConnectApplication.java
@@ -0,0 +1,13 @@
+package com.secure.connect.secure_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secure/connect/secure_connect/SecureConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secure.connect.secure_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
